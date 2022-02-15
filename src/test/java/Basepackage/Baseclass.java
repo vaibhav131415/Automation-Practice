34cf65d5895687f5cd39446d32ccbf83b946463a
VaibhavPrabhuGaonkar <vprabhugaonkar13@gmail.com>
2022-02-15 16:43:11 +0530

    Extent Report take screenshot code

--- src/test/java/Basepackage/Baseclass.java
+++ src/test/java/Basepackage/Baseclass.java
@@ -2,18 +2,61 @@ package Basepackage;
 
 
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class Baseclass {
 	public WebDriver driver;
+	public ExtentHtmlReporter htmlReporter;
+	public ExtentReports extent;
+	public ExtentTest test;
 
 	@BeforeSuite (alwaysRun=true)
 	public void Configuration() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\AVV4MD744\\git\\repository2\\Sampleproject\\Chromdriver\\chromedriver.exe");
+		
+		
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\Automation Report\\ExtentReport.html");
+		extent = new ExtentReports();  //create object of ExtentReports
+		extent.attachReporter(htmlReporter);
+
+		htmlReporter.config().setDocumentTitle("Rediif Test Report"); // Tittle of Report
+		htmlReporter.config().setReportName("Rediif Test Report"); // Name of the report
+		htmlReporter.config().setTheme(Theme.DARK);//Default Theme of Report
+
+		// General information releated to application
+		extent.setSystemInfo("Application Name", "Rediff Test");
+		extent.setSystemInfo("User Name", "Vaibhav Prabhugaonkar");
+		extent.setSystemInfo("Envirnoment", "Satging");
+		htmlReporter.config().setAutoCreateRelativePathMedia(true);
+		
+		
+		
+		
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sprab\\git\\Automation-Practice\\Chromdriver\\chromedriver.exe");
 
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -26,6 +69,41 @@ public class Baseclass {
 	public void afterconfig() {
 
 		driver.quit();
+		extent.flush();
+	}
+	
+	public static String getScreenShot(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		// after execution, you could see a folder "FailedTestsScreenshots" under src folder
+		String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+		}
+	
+	@AfterMethod
+	public void getResult(ITestResult result) throws Exception{
+	if(result.getStatus() == ITestResult.FAILURE){
+	//MarkupHelper is used to display the output in different colors
+	test.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+	test.log(Status.FAIL, MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+	//To capture screenshot path and store the path of the screenshot in the string "screenshotPath"
+	//We do pass the path captured by this method in to the extent reports using "logger.addScreenCapture" method.
+	//String Scrnshot=TakeScreenshot.captuerScreenshot(driver,"TestCaseFailed");
+	String screenshotPath = getScreenShot(driver, result.getName());
+	//To add it in the extent report
+	test.fail("Test Case Failed Snapshot is below " + test.addScreenCaptureFromPath(screenshotPath));
+	}
+	else if(result.getStatus() == ITestResult.SKIP){
+	test.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
+	}
+	else if(result.getStatus() == ITestResult.SUCCESS)
+	{
+	test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" Test Case PASSED", ExtentColor.GREEN));
+	}
+	
 	}
 
 
--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -3,6 +3,9 @@ package Tests;
 
 
 
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
 import java.time.Duration;
 
 
@@ -13,6 +16,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
 import Basepackage.Baseclass;
 
 
@@ -21,26 +31,28 @@ public class Login extends Baseclass{
 
 	@Test(priority = 1 , description = "To Verify the Title of the Page")
 	public void TitleCheck() {
-
-		String actual = driver.getTitle();
-		String Expected = "Rediff.com: News | Rediffmail | Stock Quotes | Shopping";
-		if(actual.equals(Expected)) {
-			System.out.println("Title Verified");
-		}else {
-			System.out.println("Title not verified");
-		}
-
-
+         test = extent.createTest("Verify the homepage title");
+//		String actual = driver.getTitle();
+//		String Expected = "Rediff.com: News | Rediffmail | Stock Quotes | Shopping";
+		SoftAssert softassert = new SoftAssert();
+		softassert.assertEquals(driver.getTitle(), "Rediff.com: News | Rediffmail | Stock Quotes | Shoppingpingngngngngngng / kkk");
+      softassert.assertAll();
 	}
 
 
 	@Test(priority = 2, description = "Click on money link and assert")
 	public void moneylink() {
+		test = extent.createTest("Verify if user is able to navigate to moneypage");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		try {
 		driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/a[2]")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		}catch(Exception e) {
+			test.log(Status.FAIL,MarkupHelper.createLabel("failed", ExtentColor.RED));
+			throw e;
+		}
 		//WebElement actual1 = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div[1]/a/span"));
-		
+		test.pass("user navigation passed");
 		
        
 		
@@ -54,25 +66,25 @@ public class Login extends Baseclass{
 
 
 	@Test(priority = 3, description = "To verify valid login scenario")
-	public void ValidLogin() throws InterruptedException {
+	public void ValidLogin() throws InterruptedException, IOException {
+		
+		 test = extent.createTest("Verify if user is able to login");
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 		driver.findElement(By.xpath("//a[contains(text(),'Sign In')]")).click();
 		driver.findElement(By.id("useremail")).sendKeys("vprabhugaonkar13@gmail.com");
 		driver.findElement(By.id("userpass")).sendKeys("Test@123");
 		driver.findElement(By.id("rememberflag")).click();
 		driver.findElement(By.id("loginsubmit")).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-	WebElement ele1 = 	driver.findElement(By.xpath("//*[@id=\"username\"]/a"));
-		try{
-			assertion.assertEquals(ele1, "Vaibhav");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		
-	    System.out.println("Assert passed for after sign in check");
-		}catch(AssertionError e1) {
-			System.out.println("Assert passed for after sign in check");
-			throw e1;
+		
+	WebElement ele1 = 	driver.findElement(By.xpath("//*[@id=\"username\"]/a"));
+//	String ele2 = "Test";
+	assertion.assertEquals(ele1, "vaibhav");
+		
 			
-			
-		}
+		
 		
 		
 
